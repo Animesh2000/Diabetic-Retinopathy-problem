0445ac78a8f89c0ff3e44c39495003cf54050912
Animesh2000 <42893996+Animesh2000@users.noreply.github.com>
2020-03-30 11:19:04 +0530

    Create model.py
    contains the code of my Kaggle notebook for predicting diabetic retinopathy

---
+++ model.py
@@ -0,0 +1,109 @@
+import numpy as np # linear algebra
+import pandas as pd
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+import cv2
+from PIL import Image
+import numpy as np
+from keras import layers
+from keras.applications import DenseNet121
+from keras.callbacks import Callback, ModelCheckpoint
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.optimizers import Adam
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import cohen_kappa_score, accuracy_score
+import scipy
+import tensorflow as tf
+from tqdm import tqdm
+from tensorflow.python import keras
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense,Flatten,Conv2D,Dropout
+
+img_rows,img_cols = 224,224
+num_classes = 5
+
+import cv2
+from PIL import Image
+
+def preprocess_image(image_path, desired_size=224):
+    im = Image.open(image_path)
+    im = im.resize((desired_size, )*2, resample=Image.LANCZOS)
+    
+    return im
+   
+from tqdm import tqdm
+N = train_df.shape[0]
+x_train = np.empty((N, 224, 224, 3), dtype=np.uint8)
+
+for i, image_id in enumerate(tqdm(train_df['id_code'])):
+    x_train[i, :, :, :] = preprocess_image(
+        f'../input/aptos2019-blindness-detection/train_images/{image_id}.png'
+    )
+N = test_df.shape[0]
+x_test = np.empty((N, 224, 224, 3), dtype=np.uint8)
+
+for i, image_id in enumerate(tqdm(test_df['id_code'])):
+    x_test[i, :, :, :] = preprocess_image(
+        f'../input/aptos2019-blindness-detection/test_images/{image_id}.png'
+    )
+y_train = pd.get_dummies(train_df['diagnosis']).values
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+
+densenet = DenseNet121(
+    weights='../input/densenet-keras/DenseNet-BC-121-32-no-top.h5',
+    include_top=False,
+    input_shape=(224,224,3)
+)
+def build_model():
+    model = Sequential()
+    model.add(densenet)
+    model.add(layers.GlobalAveragePooling2D())
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(5, activation='sigmoid'))
+    
+    model.compile(
+        loss='binary_crossentropy',
+        optimizer=Adam(lr=0.00005),
+        metrics=['accuracy']
+    )
+    
+    return model
+    
+model = build_model()
+model.summary()    
+
+BATCH_SIZE = 32
+
+x_train, x_val, y_train, y_val = train_test_split(
+    x_train, y_train, 
+    test_size=0.15, 
+    random_state=2019
+)
+
+def create_datagen():
+    return ImageDataGenerator(
+        zoom_range=0.15,  # set range for random zoom
+        # set mode for filling points outside the input boundaries
+        fill_mode='constant',
+        cval=0.,  # value used for fill_mode = "constant"
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=True,  # randomly flip images
+    )
+
+data_generator = create_datagen().flow(x_train, y_train, batch_size=BATCH_SIZE, seed=2019)
+
+ model.fit_generator(
+    data_generator,
+    steps_per_epoch=x_train.shape[0] / BATCH_SIZE,
+    epochs=15,
+    validation_data=(x_val, y_val),validation_steps = 1
+)
